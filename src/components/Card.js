3a4af332b10db8af05dd18bc1f7da83b377f2764
Rajesh <Rajeshniraj4@gmail.com>
2021-11-17 14:11:40 +0530

    new test

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Card = ({ name, email, id }) => {    
+    return(
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="robots" 
+                    src={`https://robohash.org/${id}?200x200`}/>
+            <div>
+                <h2>{name}</h2>
+            </div>
+            <p>{email}</p>
+        </div>
+        
+    )
+}
+
+// class Card extends Component {
+//     render(){
+//         return(
+//             <div>
+
+//             </div>
+//         )
+//     }
+// }
+
+export default Card;
