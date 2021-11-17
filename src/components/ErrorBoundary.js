ad0d310691471a6a51994039bf83e81ed77b07ef
Rajesh <Rajeshniraj4@gmail.com>
2021-11-17 14:17:58 +0530

    Erro rec

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -2,6 +2,7 @@ import React , {Component} from "react";
 
 class ErrorBoundary extends Component{
     constructor(props){
+        super(props);
         this.state = {
             hasError : false
         }
